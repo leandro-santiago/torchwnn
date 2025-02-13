89500fa605729d06b61b3cdb0651ba7e40aec9db
Leandro Santiago <leandrosantiagoa@gmail.com>
2025-02-13 02:32:36 -0300

    Configuring setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ ext_modules.append(CppExtension("torchwnn.cpp.functional", [os.path.join(cpp_dir
 
 setup(
     name="torchwnn",
-    version="1.0.0",
+    version="0.0.0",
     author="Leandro Santiago de Araújo",
     description="Torcwnn is a Python library for Weightless Neural Network",
     long_description=open("README.md").read(),
